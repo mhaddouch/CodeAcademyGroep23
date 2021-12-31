e6cac1f92d3c2f08cb1f6a696ddff09ea5e7d443
mvdsommen1 <94703677+mvdsommen1@users.noreply.github.com>
2021-12-31 16:34:22 +0100

    Getters en Setters toegevoegd

--- CodeAcedemyCode/src/Domain/Registration.java
+++ CodeAcedemyCode/src/Domain/Registration.java
@@ -1,6 +1,15 @@
 package Domain;
+import java.util.Date;
 
 public class Registration {
 private Date registrationate;
+
+public Date getRegistrationate() {
+    return registrationate;
+}
+
+public void setRegistrationate(Date registrationate) {
+    this.registrationate = registrationate;
+}
 
 }
