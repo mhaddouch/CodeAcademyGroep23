e07ac86ad811a6f0ab54374fcc9eac394a3822ca
mvdsommen1 <94703677+mvdsommen1@users.noreply.github.com>
2021-12-31 16:33:36 +0100

    Getters and Setters toegevoegd

--- CodeAcedemyCode/src/Domain/Module.java
+++ CodeAcedemyCode/src/Domain/Module.java
@@ -4,5 +4,30 @@ public class Module {
 private String title;
 private String version;
 private String description;
-private String emailContactPerson
+private String emailContactPerson;
+
+public String getTitle() {
+    return title;
+}
+public void setTitle(String title) {
+    this.title = title;
+}
+public String getVersion() {
+    return version;
+}
+public void setVersion(String version) {
+    this.version = version;
+}
+public String getDescription() {
+    return description;
+}
+public void setDescription(String description) {
+    this.description = description;
+}
+public String getEmailContactPerson() {
+    return emailContactPerson;
+}
+public void setEmailContactPerson(String emailContactPerson) {
+    this.emailContactPerson = emailContactPerson;
+}
 }
--- CodeAcedemyCode/src/Domain/Webcast.java
+++ CodeAcedemyCode/src/Domain/Webcast.java
@@ -11,4 +11,47 @@ private String nameSpeaker;
 private String organisation;
 private String description;
 
+public String getTitle() {
+    return title;
+}
+public void setTitle(String title) {
+    this.title = title;
+}
+public String getURL() {
+    return URL;
+}
+public void setURL(String uRL) {
+    URL = uRL;
+}
+public Date getDate() {
+    return date;
+}
+public void setDate(Date date) {
+    this.date = date;
+}
+public int getDuration() {
+    return duration;
+}
+public void setDuration(int duration) {
+    this.duration = duration;
+}
+public String getNameSpeaker() {
+    return nameSpeaker;
+}
+public void setNameSpeaker(String nameSpeaker) {
+    this.nameSpeaker = nameSpeaker;
+}
+public String getOrganisation() {
+    return organisation;
+}
+public void setOrganisation(String organisation) {
+    this.organisation = organisation;
+}
+public String getDescription() {
+    return description;
+}
+public void setDescription(String description) {
+    this.description = description;
+}
+
 }
