3f8255df9bb277c8618232b18ec9cceee5ea7d87
mygitname <mygitemail>
2021-12-31 16:37:31 +0100

    commit domain classes

--- CodeAcedemyCode/src/Domain/ContentItem.java
+++ CodeAcedemyCode/src/Domain/ContentItem.java
@@ -1,5 +1,38 @@
 package Domain;
 
+import java.util.Date;
+
 public abstract class ContentItem {
+    private int ContentItem;
+    private Date date;
+    private String status;
+
+    public int getContentItem() {
+        return ContentItem;
+    }
+
+    public void setContentItem(int contentItem) {
+        ContentItem = contentItem;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getPercentage(double percentage) {
+        return percentage;
+    }
 
 }
--- CodeAcedemyCode/src/Domain/Course.java
+++ CodeAcedemyCode/src/Domain/Course.java
@@ -1,5 +1,55 @@
 package Domain;
 
-public class Course {
+import java.util.ArrayList;
 
+public class Course {
+    private String courseName;
+    private String subject;
+    private String introductionText;
+    private String level;
+    private ArrayList<Course> interest = new ArrayList<>();
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getIntroductionText() {
+        return introductionText;
+    }
+
+    public void setIntroductionText(String introductionText) {
+        this.introductionText = introductionText;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public ArrayList<Course> getInterest() {
+        return interest;
+    }
+
+    public void setInterest(ArrayList<Course> interest) {
+        this.interest = interest;
+    }
+
+    public void addCourse(Course course) {
+        this.interest.add(course);
+    }
 }
